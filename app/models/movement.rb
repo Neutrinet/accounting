ed9371c675e08e52c6dda356053deb4cbcee1414
Thomas Balthazar <thomas@balthazar.info>
2018-09-08 15:27:54 +0200

    Create a Movement from a row

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,4 +1,16 @@
 class Movement < ApplicationRecord
   validates_uniqueness_of :number, scope: :date
   validates_presence_of :number, :date, :amount, :iban, :movement_type, :raw
+
+  def self.from_row(movement_row)
+    create(
+      number: movement_row.number,
+      date: movement_row.date,
+      amount: movement_row.amount,
+      iban: movement_row.iban,
+      communication: movement_row.communication,
+      movement_type: movement_row.movement_type,
+      raw: movement_row.row
+    )
+  end
 end
--- spec/models/movement_spec.rb
+++ spec/models/movement_spec.rb
@@ -45,4 +45,27 @@ RSpec.describe Movement, type: :model do
     expect_required_field(:movement_type)
     expect_required_field(:raw)
   end
+
+  describe ".from_row" do
+    let(:csv_row) { csv_rows("recordbank_vpn").first }
+    let(:movement_row) { MovementRow.new(csv_row) }
+
+    it "creates a movement from a CSV row" do
+      expect do
+        movement = Movement.from_row(movement_row)
+      end.to change { Movement.count }.by(1)
+    end
+
+    it "maps all the fields correctly" do
+      movement = Movement.from_row(movement_row)
+
+      expect(movement.number).to eql(movement_row.number)
+      expect(movement.date).to eql(DateTime.parse(movement_row.date).utc)
+      expect(movement.amount).to eql(movement_row.amount)
+      expect(movement.iban).to eql(movement_row.iban)
+      expect(movement.communication).to eql(movement_row.communication)
+      expect(movement.movement_type).to eql(movement_row.movement_type)
+      expect(movement.raw).to eql(movement_row.row.to_s)
+    end
+  end
 end
