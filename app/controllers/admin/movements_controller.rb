80991a3636d4978e25cd1d39b509999a5195a896
Thomas Balthazar <thomas@balthazar.info>
2018-09-19 22:47:48 +0200

    Start implementing movement edit form

--- app/controllers/admin/movements_controller.rb
+++ app/controllers/admin/movements_controller.rb
@@ -4,4 +4,12 @@ class Admin::MovementsController < Admin::BaseController
     @unknown_movements = Movement.unknown
     @no_iban_movements = Movement.where(iban: nil).where.not(movement_type: "vpn")
   end
+
+  def edit
+    @movement = Movement.find(params[:id])
+  end
+
+  def update
+    redirect_to action: :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
     resource :imports, only: [:new, :create]
-    resources :movements, only: [:index]
+    resources :movements, only: [:index, :edit, :update]
   end
 end
